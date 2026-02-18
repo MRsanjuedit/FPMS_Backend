156a86f0b00065f76676cc7d19f5110583ea425f
MRsanjuedit <tallurisanju1@gmail.com>
2026-02-18 12:12:46 +0530

    Removed serviceAccount.json replace with .env

--- config/firebase.js
+++ config/firebase.js
@@ -1,21 +1,26 @@
 
 import admin from 'firebase-admin';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const serviceAccountPath = path.join(__dirname, '..', 'serviceAccountKey.json');
+dotenv.config();
 
 let auth = null;
 let db = null;
 
 try {
-  const serviceAccount = JSON.parse(
-    fs.readFileSync(serviceAccountPath, 'utf8')
-  );
+  const serviceAccount = {
+    type: "service_account",
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+  };
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
